b71deb5202e8c9d19c4267a22f5d8d7f0ddfc5dd
rlaehrla <rlaehrla@naver.com>
2024-01-03 12:44:21 +0900

    2024_01_03 코드 추가

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -43,8 +43,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests(c -> {
            c.requestMatchers("/mypage/**")
                    .authenticated() // 회원 전용
-                   .requestMatchers("/admin/**")
-                   .hasAnyAuthority("ADMIN", "MANAGER")
+                   //.requestMatchers("/admin/**")
+                   //.hasAnyAuthority("ADMIN", "MANAGER")
                    .anyRequest().permitAll(); // 그외 모든 페이지는 모두 접근 가능
         });
 
