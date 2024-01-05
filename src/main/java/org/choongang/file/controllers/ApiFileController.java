77b2d06d691b3633383caadd730d348f0eec9676
rlaehrla <rlaehrla@naver.com>
2024-01-05 10:47:38 +0900

    2024_01_05 코드 추가

---
+++ src/main/java/org/choongang/file/controllers/ApiFileController.java
@@ -0,0 +1,24 @@
+package org.choongang.file.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.ExceptionRestProcessor;
+import org.choongang.file.service.FileUploadService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/file")
+@RequiredArgsConstructor
+public class ApiFileController implements ExceptionRestProcessor {
+
+    private final FileUploadService uploadService;
+
+    public void upload(@RequestParam("file") MultipartFile[] files,
+                       @RequestParam(name="gid", required = false) String gid,
+                       @RequestParam(name="location", required = false) String location) {
+
+            uploadService.upload(files, gid, location);
+    }
+}
