fc4e857067fb6d3493cd0682c22e1270573a18db
rlaehrla <rlaehrla@naver.com>
2024-01-08 17:24:20 +0900

    2024_01_08 코드 추가

--- src/main/java/org/choongang/admin/board/BoardController.java
+++ src/main/java/org/choongang/admin/board/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller("adminBoardController")
@@ -84,6 +85,8 @@ public class BoardController implements ExceptionProcessor {
         String pageTitle = "게시판 목록";
         mode = StringUtils.hasText(mode) ? mode : "list";
 
+
+
         if (mode.equals("add")) {
             pageTitle = "게시판 등록";
 
@@ -92,9 +95,19 @@ public class BoardController implements ExceptionProcessor {
 
         } else if (mode.equals("posts")) {
             pageTitle = "게시글 관리";
+
+        }
+
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+        if (mode.equals("add") || mode.equals("edit")) { // 게시판 등록 또는 수정
+            addCommonScript.add("ckeditor5/ckeditor");
+            addScript.add("board/form");
         }
 
         model.addAttribute("pageTitle", pageTitle);
         model.addAttribute("subMenuCode", mode);
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addScript", addScript);
     }
 }
