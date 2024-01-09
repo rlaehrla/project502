e3a93355b17adacd98e28a497beddff0157a9f31
rlaehrla <rlaehrla@naver.com>
2024-01-09 14:16:44 +0900

    2024_01_09 코드 추가

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -60,6 +60,8 @@ public class SecurityConfig {
 
         /* 인가 설정 E - 접근 통제 */
 
+        http.headers(c -> c.frameOptions(f -> f.sameOrigin())); // 같은 출처는 접근 가능
+
         return http.build();
     }
 
